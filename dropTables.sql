ff98e00daaba040ccdc53e74741efd0a0a5bb883
hanyilMilo <143642524+hanyilMilo@users.noreply.github.com>
2023-09-11 17:50:34 -0400

    Add files via upload

---
+++ dropTables.sql
@@ -0,0 +1,19 @@
+ALTER TABLE ALBUMS DROP CONSTRAINT covered_by_photo;
+
+drop table Cities CASCADE CONSTRAINTS;
+
+drop table Users CASCADE CONSTRAINTS;
+
+drop table Programs CASCADE CONSTRAINTS;
+
+drop table Education;
+
+drop table User_Events;
+
+drop table Participants;
+
+drop table Albums CASCADE CONSTRAINTS;
+
+drop table Photos CASCADE CONSTRAINTS;
+
+drop table Tags;
